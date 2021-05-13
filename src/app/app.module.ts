53c5a9764747b3dab0ab092b7eb5a2bf719faeb5
Biren Sarvaiya <birensarvaiya24@gmail.com>
2021-05-12 23:45:35 -0400

    initial commit. navbar and static table done. Dynamic Table NOT done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavbartopComponent } from './navbartop/navbartop.component';
+import { PetsTableComponent } from './pets-table/pets-table.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbartopComponent,
+    PetsTableComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/pets-table/pets-table.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pets-table',
+  templateUrl: './pets-table.component.html',
+  styleUrls: ['./pets-table.component.css']
+})
+export class PetsTableComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
